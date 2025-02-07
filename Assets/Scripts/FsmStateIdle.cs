f5835301770cb0e5e838c07c1faa0bb9e70a6d0a
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2025-02-07 14:33:30 +0300

    The finite state machine pattern is implemented

---
+++ Assets/Scripts/FsmStateIdle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FsmStateIdle : FsmState
+{
+    public FsmStateIdle(Fsm fsm) : base(fsm) { }
+
+    public override void Enter()
+    {
+        Debug.Log("Idle state [ENTER]");
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Idle state [EXIT]");
+    }
+
+    public override void Update()
+    {
+        Debug.Log("Idle state [UPDATE]");
+
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            Fsm.SetState<FsmStateWalk>();
+        }
+    }
+}
---
+++ Assets/Scripts/FsmStateMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class FsmStateMovement : FsmState
+{
+    protected readonly Transform Transform;
+    protected readonly float Speed;
+
+    public FsmStateMovement(Fsm fsm, Transform transform, float speed) : base(fsm)
+    {
+        Transform = transform;
+        Speed = speed;
+    }
+
+    public override void Enter()
+    {
+        Debug.Log($"Movement ({this.GetType().Name}) state [ENTER]");
+    }
+
+    public override void Exit()
+    {
+        Debug.Log($"Movement ({this.GetType().Name}) state [EXIt]");
+    }
+
+    public override void Update()
+    {
+        Debug.Log($"Movement ({this.GetType().Name}) state [UPDATE] with speed: {Speed}");
+
+        var inputDirection = ReadInput();
+
+        if(inputDirection.sqrMagnitude == 0f)
+        {
+            Fsm.SetState<FsmStateIdle>();
+        }
+
+        Move(inputDirection);
+    }
+
+    protected virtual Vector2 ReadInput()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        return input;
+    }
+
+    protected virtual void Move(Vector2 inputDirection)
+    {
+        Transform.position += new Vector3(inputDirection.x, 0f, inputDirection.y) * (Speed * Time.deltaTime);
+    }
+}
+
